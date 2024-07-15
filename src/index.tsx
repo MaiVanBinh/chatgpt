c25c0ae234dbb5fec0276b0bf467eb8b31dad8d6
MaiVanBinh <55869650+MaiVanBinh@users.noreply.github.com>
2024-07-15 11:46:04 +0700

    add homepage, base url

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './page/App';
 import reportWebVitals from './reportWebVitals';
 import axios from "axios";
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 
@@ -11,7 +12,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    {/* http://ec2-3-34-140-68.ap-northeast-2.compute.amazonaws.com:12345/frontend_mymy_demo */}
+    <BrowserRouter
+      basename="/frontend_mymy_demo" // Add basename prop
+    >
     <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
